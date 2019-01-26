2582b543a22c778c76c3e88806ae4a943a2f7a7a
egor066416 <066416@gmail.com>
2019-01-26 22:48:11 +0200

    dashboard- add two elements, add elementCheckbox

--- src/app/elements/models/elementCheckbox.ts
+++ src/app/elements/models/elementCheckbox.ts
@@ -4,9 +4,10 @@ import { CheckboxComponent } from '../elements';
 export class ElementCheckbox extends Element {
   isChecked: boolean;
 
-  constructor () {
+  constructor(isChecked?: boolean) {
     super();
     this.component = CheckboxComponent;
     this.key = 'checkbox' + Date.now();
+    this.isChecked = isChecked ? isChecked : false;
   }
 }
